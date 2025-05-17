afdd49be6889565e28bf7ded9f31996062d37a1d
TateJohnston <tate1johnston@gmail.com>
2025-05-17 13:43:33 +0800

    first commit

---
+++ mySQL_app/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+let dbConnect = require("./dbConnect");
+const app = express();
+let userRoutes = require("./routes/userRoutes");
+
+// parse requests of content-type - application / json;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MongoDB application." });
+});
+
+// set port, listen for requests
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
